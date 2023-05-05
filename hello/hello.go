53b0069664ce0aa9f5c05e01b051e09b209c55dd
Ryan Soeder <ryan@mightily.com>
2023-05-05 13:41:54 -0700

    initial commit

---
+++ hello/hello.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	// Set properties of the predefined Logger, including the log entry prefix and a flag to disable printing the time, source file, and line number.
+	log.SetPrefix("greeting: ")
+	log.SetFlags(0)
+
+    // Request a greeting message
+    // message, err := greetings.Hello("tim")
+
+	// A slice of names.
+	names := []string{"Tim", "Tom", "Terry"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+
+	// If an error was returned, print it to the console and exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no error was returned, print the returned message to the console.
+    fmt.Println(messages)
+}
